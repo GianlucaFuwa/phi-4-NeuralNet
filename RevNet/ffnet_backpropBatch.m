8aff5cccc0366a4bb8e78d9d7b8efe61afc1630e
GianlucaFuwa <51165364+GianlucaFuwa@users.noreply.github.com>
2021-07-08 18:51:39 +0200

    Update ffnet_backpropBatch.m

--- RevNet/ffnet_backpropBatch.m
+++ RevNet/ffnet_backpropBatch.m
@@ -1,5 +1,5 @@
 % compute gradient of loss wrt weights
-function [dEdwF,dEdwG] = ffnet_backpropBatch(net, goal)
+function [dEdwF,dEdwG] = net_backpropBatch(net, goal)
 dEdwF = cell(1,2);
 dEdwG = cell(1,2);
 
@@ -12,25 +12,25 @@ y2_grad = y_grad(len+1:end);
 % determine number of weight matrices and compute Gradients of Net. Output
 if net.Gnet.Nlayers == 3
     % calculate jacobian of G wrt y1
-    net.Gnet = ffnet_dNetdI(net.Gnet,true);
-    net.Fnet = ffnet_dNetdI(net.Fnet,false);
+    net.Gnet = net_dNetdI(net.Gnet,true);
+    net.Fnet = net_dNetdI(net.Fnet,false);
     % calculate jacobian of each network's output wrt weights
-    net.Gnet = ffnet_dNetdw(net.Gnet);
-    net.Fnet = ffnet_dNetdw(net.Fnet);    
+    net.Gnet = net_dNetdw(net.Gnet);
+    net.Fnet = net_dNetdw(net.Fnet);    
 elseif net.Gnet.Nlayers == 4
     % calculate jacobian of G wrt y1
-    net.Gnet = ffnet_dNetdI2(net.Gnet,true);
-    net.Fnet = ffnet_dNetdI2(net.Fnet,false);
+    net.Gnet = net_dNetdI2(net.Gnet,true);
+    net.Fnet = net_dNetdI2(net.Fnet,false);
     % calculate jacobian of each network's output wrt weights
-    net.Gnet = ffnet_dNetdw2(net.Gnet);
-    net.Fnet = ffnet_dNetdw2(net.Fnet); 
+    net.Gnet = net_dNetdw2(net.Gnet);
+    net.Fnet = net_dNetdw2(net.Fnet); 
 elseif net.Gnet.Nlayers == 5
     % calculate jacobian of G wrt y1
-    net.Gnet = ffnet_dNetdI3(net.Gnet,true);
-    net.Fnet = ffnet_dNetdI3(net.Fnet,false);
+    net.Gnet = net_dNetdI3(net.Gnet,true);
+    net.Fnet = net_dNetdI3(net.Fnet,false);
     % calculate jacobian of each network's output wrt weights
-    net.Gnet = ffnet_dNetdw3(net.Gnet);
-    net.Fnet = ffnet_dNetdw3(net.Fnet);    
+    net.Gnet = net_dNetdw3(net.Gnet);
+    net.Fnet = net_dNetdw3(net.Fnet);    
 end
  
 % calculate gradients of cost wrt weights
