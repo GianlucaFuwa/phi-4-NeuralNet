5f6e66700607e0cf518e9e4e18154047b6bcdfa1
GianlucaFuwa <51165364+GianlucaFuwa@users.noreply.github.com>
2021-07-08 19:11:35 +0200

    Update testset_gen.m

--- testset_gen.m
+++ testset_gen.m
@@ -4,11 +4,11 @@ beta = 0.6586246;
 lambda = 0.25;
 Nprod = 10000;
 
-%create training set
+% create training set
 %[~,~,trainsetrot] = phi4MC_rotteacher(L,M,beta,lambda,true);
 
-%test NN on exam
-[e,s,a] = phi4MC_rotexam(L,M,beta,lambda,Adam8_400rotS,Nprod,18);
+% evaluate performance of Network in Simulation
+[e,s,a] = phi4Net_rot(L,M,beta,lambda,Adam8_400rotS,Nprod,18);
 [me,de,dde,taue,dtaue]=UWerr(e);
 %[ms,ds,dds,taus,dtaus]=UWerr(s./L^2/2); % action per link
 
