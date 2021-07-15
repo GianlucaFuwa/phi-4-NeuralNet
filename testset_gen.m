4c066e5d5c6158f4a210a3ca5eb593734a3f7fdf
GianlucaFuwa <51165364+GianlucaFuwa@users.noreply.github.com>
2021-07-16 00:47:07 +0200

    Update testset_gen.m

--- testset_gen.m
+++ testset_gen.m
@@ -8,8 +8,10 @@ Nprod = 10000;
 %[~,~,trainsetrot] = phi4MC_rotteacher(L,M,beta,lambda,true);
 
 % evaluate performance of Network in Simulation
-[e,s,a] = phi4Net_rot(L,M,beta,lambda,Net,Nprod,18);
-[me,de,dde,taue,dtaue]=UWerr(e);
+[e,s,G0,chi2,a] = phi4Net_rot(L,M,beta,lambda,Net,Nprod,18);
+[me,de,dde,taue,dtaue]=UWerr(e); % energy
 %[ms,ds,dds,taus,dtaus]=UWerr(s./L^2/2); % action per link
+%[mG0,dG0,ddG0,tauG0,dtauG0]=UWerr(G0); % G(0)
+%[mchi2,dchi2,ddchi2,tauchi2,dtauchi2]=UWerr(chi2); % susceptibility
 
 
